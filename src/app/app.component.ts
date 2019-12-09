6f2671e428e33ce9ffa7f60b61ffc9f8260a8319
Eribio <erika.biondi@student.univaq.it>
2019-12-09 15:00:36 +0100

    vai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,61 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import {Platform} from '@ionic/angular';
+import {NavController, Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 import {TranslateService} from '@ngx-translate/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Utente} from './model/utente.model';
+import {LinguaService} from './services/lingua.service';
+import {UtenteService} from './services/utente.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    private u = new Utente();
+    private utente$: BehaviorSubject<Utente>;
+    private isLoggedIn$: Observable<boolean>;
+
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar,
-        private translate: TranslateService
+        private translate: TranslateService,
+        private linguaService: LinguaService,
+        private utenteService: UtenteService,
+        private navController: NavController,
+        private router: Router
     ) {
         this.initializeApp();
     }
 
+
+    public appPages = [
+        {
+            title: 'IMPOSTAZIONI',
+            url: '/settings',
+            icon: 'settings'
+        },
+
+    ];
+
+    ngOnInit(): void {
+
+        this.utente$ = this.utenteService.getUtente();
+        this.isLoggedIn$ = this.utenteService.isLogged();
+    }
+
     initializeApp() {
         this.platform.ready().then(() => {
-            this.statusBar.styleDefault();
-            this.splashScreen.hide();
+            this.utenteService.getUtente().subscribe(res => {
+                this.u = res;
+            });
+            // this.statusBar.styleDefault();
+            // this.splashScreen.hide();
             this.initTranslate();
         });
     }
@@ -31,5 +63,21 @@ export class AppComponent {
     initTranslate() {
         this.translate.setDefaultLang('it');
         this.translate.use('it');
+        // Set the default language for translation strings, and the current language.
+        const linguaPreferita = this.linguaService.getLinguaPreferita();
+        this.translate.setDefaultLang(linguaPreferita);
+        this.linguaService.getLinguaAttuale().subscribe((lingua: string) => {
+            if (lingua != null) {
+                this.translate.use(lingua);
+            } else {
+                this.translate.use(linguaPreferita);
+                this.linguaService.updateLingua(linguaPreferita);
+            }
+        });
+    }
+
+    logout() {
+        this.utenteService.logout();
+
     }
 }
